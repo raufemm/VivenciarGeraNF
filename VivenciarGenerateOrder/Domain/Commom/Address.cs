cd421aa9d27ec352482479fe5b99a6c242968433
Raufe <raufe.m@gmail.com>
2022-09-30 18:25:37 -0300

    Adding commom classes like Address and ValueObject

---
+++ VivenciarGenerateOrder/Domain/Commom/Address.cs
@@ -0,0 +1,42 @@
+﻿using VivenciarGenerateOrder.Domain.Enum;
+
+namespace VivenciarGenerateOrder.Domain.Commom
+{
+    public class Address : ValueObject<Address>
+    {
+        public Address(string street, string number, string moreIfo, string city, BrazilianStates state, string zipCode)
+        {
+            Street = street;
+            Number = number;
+            MoreIfo = moreIfo;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+        }
+
+        public string Street { get; private set; }
+        public string Number { get; private set; }
+        public string MoreIfo { get; private set; }
+        public string City { get; private set; }
+        public BrazilianStates State { get; private set; }
+        public string ZipCode { get; private set; }
+
+        protected override bool EqualsCore(Address other)
+        {
+            return ToString() == other.ToString();
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return ToString().GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(Street))
+                return "";
+
+            return $"{Street}, {Number} / {MoreIfo}";
+        }
+    }
+}
---
+++ VivenciarGenerateOrder/Domain/Commom/ValueObject.cs
@@ -0,0 +1,42 @@
+﻿namespace VivenciarGenerateOrder.Domain.Commom
+{
+    public abstract class ValueObject<T> where T : ValueObject<T>
+    {
+        public override bool Equals(object valueObject)
+        {
+            var _valueObject = valueObject as T;
+
+            if (ReferenceEquals(_valueObject, null))
+            {
+                return false;
+            }
+
+            return EqualsCore(_valueObject);
+        }
+
+        protected abstract bool EqualsCore(T other);
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        protected abstract int GetHashCodeCore();
+
+        public static bool operator ==(ValueObject<T> objectA, ValueObject<T> objectB)
+        {
+            if (ReferenceEquals(objectA, null) && ReferenceEquals(objectB, null))
+                return true;
+
+            if (ReferenceEquals(objectA, null) || ReferenceEquals(objectB, null))
+                return false;
+
+            return objectA.Equals(objectB);
+        }
+
+        public static bool operator !=(ValueObject<T> objectA, ValueObject<T> objctB)
+        {
+            return !(objectA == objctB);
+        }
+    }
+}
